9b7d2f2d087fd4e0d0d5b62ebd6ae6326ef1bbfb
vvarun14 <varunsai1411@gmail.com>
2026-06-22 22:12:58 +0530

    7. react frontend intialisation

---
+++ superEats-frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4x1 font-bold text-orange-500">SuperEats</h1>
+    </div>
+  );
+}
+
+export default App;
